1ff5430f9e109d7472e767ede555edb869fadcb3
cd903 <45642623+cd903@users.noreply.github.com>
2020-02-01 21:44:06 -0600

    Day 4

--- weightcalc/scripts.js
+++ weightcalc/scripts.js
@@ -1,6 +1,57 @@
 $(document).ready(function(){
 
-    // Validation
+    // Validation vars
+    validWeight = false;
+    validGoalWeight = false;
+    validCals = false;
+    validExercise = false;
+    validGender = false;
+    validExerciseDays = false;
+
+    // Main validation function
+    validateFields = function(){
+
+        submit = $('#submitBtn');
+
+        if(validWeight == false) {
+            submit.prop('disabled',true);
+            return false;
+        }
+
+        if(validGoalWeight == false) {
+            submit.prop('disabled',true);
+            return false;
+        }
+                
+        if(validCals == false) {
+            submit.prop('disabled',true);
+            return false;
+        }
+                
+        if(validExercise == false) {
+            submit.prop('disabled',true);
+            return false;
+        }
+                
+        if(validGender == false) {
+            submit.prop('disabled',true);
+            return false;
+        }
+                
+        if(validExerciseDays == false) {
+            submit.prop('disabled',true);
+            return false;
+        }
+
+        submit.prop('disabled',false);
+
+    }
+
+
+
+    
+
+    // Validation for each form field
     $('#currentWeight').on('change',function(){
         weight = $(this);
 
@@ -11,12 +62,14 @@ $(document).ready(function(){
         if($.isNumeric(weight.val()) && weight.val() != 0 && weight.val() != ''){
             weight.addClass('valid-border');
             validWeight = true;
+            validateFields();
             return validWeight;
         } else {
-            error = '<span id="currentWeightError" style="color:red;">Please enter a valid weight.';
+            error = '<span id="currentWeightError" class="error-msg">Please enter a valid weight.</span>';
             weight.parent().prepend(error)
             weight.addClass('error-border');
-            return false;
+            validWeight = false;
+            return validWeight;
         }
     })
 
@@ -30,62 +83,171 @@ $(document).ready(function(){
         if($.isNumeric(weight.val()) && weight.val() != 0 && weight.val() != ''){
             weight.addClass('valid-border');
             validGoalWeight = true;
+            validateFields();
             return validGoalWeight;
         } else {
-            error = '<span id="currentGoalWeightError" style="color:red;">Please enter a valid weight.';
+            error = '<span id="currentGoalWeightError" class="error-msg">Please enter a valid weight.</span>';
             weight.parent().prepend(error)
             weight.addClass('error-border');
-            return false;
+            validGoalWeight = false;
+            validateFields();
+            return validGoalWeight;
         }
     })
 
     $('#consumed').on('change',function(){
-        weight = $(this);
+        consumed = $(this);
 
         $('#consumedError').remove();
-        weight.removeClass('error-border');
-        weight.removeClass('valid-border');
+        consumed.removeClass('error-border');
+        consumed.removeClass('valid-border');
 
-        if($.isNumeric(weight.val()) && weight.val() != 0 && weight.val() != ''){
-            weight.addClass('valid-border');
+        if($.isNumeric(consumed.val()) && consumed.val() != 0 && consumed.val() != ''){
+            consumed.addClass('valid-border');
             validCals = true;
+            validateFields();
             return validCals;
         } else {
-            error = '<span id="consumedError" style="color:red;">Please enter a valid weight.';
-            weight.parent().prepend(error)
-            weight.addClass('error-border');
-            return false;
+            error = '<span id="consumedError" class="error-msg">Please enter the amount of calories you normally eat.</span>';
+            consumed.parent().prepend(error)
+            consumed.addClass('error-border');
+            validCals = false;
+            validateFields();
+            return validCals;
         }
     })
 
     $('#exercise').on('change',function(){
-        weight = $(this);
+        exercise = $(this);
 
         $('#exerciseError').remove();
-        weight.removeClass('error-border');
-        weight.removeClass('valid-border');
+        exercise.removeClass('error-border');
+        exercise.removeClass('valid-border');
 
-        if($.isNumeric(weight.val()) && weight.val() != 0 && weight.val() != ''){
-            weight.addClass('valid-border');
+        if($.isNumeric(exercise.val()) && exercise.val() != 0 && exercise.val() != ''){
+            exercise.addClass('valid-border');
             validExercise = true;
+            validateFields();
             return validExercise;
         } else {
-            error = '<span id="exerciseError" style="color:red;">Please enter a valid weight.';
-            weight.parent().prepend(error)
-            weight.addClass('error-border');
-            return false;
+            error = '<span id="exerciseError" class="error-msg">Please enter the amount of calories you normally burn.</span>';
+            exercise.parent().prepend(error)
+            exercise.addClass('error-border');
+            validExercise = false;
+            validateFields();
+            return validExercise;
         }
     })
 
     $('#gender').on('change',function(){
         gender = $(this);
 
+        $('#genderError').remove();
+        gender.removeClass('error-border');
+        gender.removeClass('valid-border');
+
         if(gender.val() !=''){
-            
+            gender.addClass('valid-border');
+            validGender = true;
+            validateFields();
+            return validGender;
         } else {
-            
+            error = '<span id="genderError" class="error-msg">Please select a gender.</span>';
+            gender.parent().prepend(error)
+            gender.addClass('error-border');
+            validGender = false;
+            validateFields();
+            return validGender;
+        }
+    })
+
+    $('#exerciseDays').on('change',function(){
+        exerciseDays = $(this);
+
+        $('#exerciseDaysError').remove();
+        exerciseDays.removeClass('error-border');
+        exerciseDays.removeClass('valid-border');
+
+        if(exerciseDays.val() !=''){
+            exerciseDays.addClass('valid-border');
+            validExerciseDays = true;
+            validateFields();
+            return validExerciseDays;
+        } else {
+            error = '<span id="exerciseDaysError" class="error-msg">Please select how many days you exercise.</span>';
+            exerciseDays.parent().prepend(error)
+            exerciseDays.addClass('error-border');
+            validExerciseDays = false;
+            validateFields();
+            return validExerciseDays;
         }
     })
     
+    $('#resetBtn').on('click',function(){
+        $('#currentWeight').val('');
+        $('#goalWeight').val('');
+        $('#consumed').val('');
+        $('#exercise').val('');
+        $('#gender').val('');
+        $('#exerciseDays').val('');
+        $('.valid-border').removeClass('valid-border');
+        $('.error-border').removeClass('error-border');
+        $('.error-msg').remove();
+        validWeight = false;
+        validGoalWeight = false;
+        validCals = false;
+        validExercise = false;
+        validGender = false;
+        validExerciseDays = false;
+        validateFields();
+    })
+
+    // Functions for setting values
+    calcTDEE = function(){
+        gender = $('#gender').val();
+
+        if(gender == 1) {
+            tdee = 12
+            return tdee;
+        } else if(gender == 2) {
+            tdee = 10
+            return tdee;
+        } else {
+            alert('Invalid selection in gender field, please click reset and try again.');
+            return false;
+        }
+    }
+
+    calcDailyBurn = function() {
+        exercise = $('#exercise').val();
+        exerciseDays = $('#exerciseDays').val();
+
+        if(exerciseDays == 7) {
+            averageBurn = exercise;
+            return averageBurn;
+        } else {
+            exerciseArray = [];
+            for(i=0;i<7;i++){
+                if(exerciseDays >= i+1){
+                    exerciseArray.push(exercise);
+                } else {
+                    exerciseArray.push(0);
+                }
+            }
+            sum = 0;
+            $.each(exerciseArray,function(){
+                sum += parseInt(this);
+            })
+            averageBurn = Math.round(sum / exerciseArray.length);
+            return averageBurn;
+        }
+    }
+
+
+    // Calcuation function 
+    $('#submitBtn').on('click',function(){
+        tdee = calcTDEE();
+        averageBurn = calcDailyBurn();
+    })
 
 })
